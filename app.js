668aa3fff76bb391d24739dc67a2b2d9d8133d95
AYUSH MISHRA <ayushm185@gmail.com>
2024-04-23 11:20:47 +0530

    updated

--- app.js
+++ app.js
@@ -14,14 +14,16 @@ config()
 
 
 const app=express()
+app.use(cors({
+   origin:[process.env.FRONTEND_URL],
+   credentials:true
+}))
 
 app.use(express.json())
 app.use(cookieParser())
 
-
 app.use(express.urlencoded({extended:true}))
 app.use(morgan('dev'))
-app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
 app.use('/api/v1/user',userRoutes)
 app.use('/api/v1/courses',courseRouter)
 app.use('/api/v1/payements',payementRoutes)
