3abccc3da3df70b6426bbaa13a91a4138daf3bce
AYUSH MISHRA <ayushm185@gmail.com>
2024-05-19 20:03:17 +0530

    middleware code setup

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,21 +1,41 @@
 import AppError from "../utils/error.util.js"
 import jwt from 'jsonwebtoken'
 
-const isLoogedIn=async(req,res,next)=>{
+// const isLoogedIn=async(req,res,next)=>{
      
-     const {token}=await req.cookies
-     console.log(token);
+//      const {token}=await req.cookies
+//      console.log(token);
 
-     if(!token){
-        return next(new AppError("Not login in",401))
-     }
+//      if(!token){
+//         return next(new AppError("Not login in",401))
+//      }
 
-     const userDetails=await jwt.verify(token,process.env.SECRET)
+//      const userDetails=await jwt.verify(token,process.env.SECRET)
 
-     req.user=userDetails
+//      req.user=userDetails
 
-     next()
-}
+//      next()
+// }
+
+const isLoogedIn = async (req, res, next) => {
+   try {
+       const { token } = req.cookies; // Assuming you're using cookie-parser middleware
+
+       console.log(token);
+
+       if (!token) {
+           throw new Error("Not logged in");
+       }
+
+       const userDetails = await jwt.verify(token, process.env.SECRET);
+
+       req.user = userDetails;
+
+       next();
+   } catch (error) {
+       return next(new AppError("Not logged in", 401));
+   }
+};
 
 const authorizedRoles=(...roles)=>async(req,res,next)=>{
    const currentUserRoles=req.user.role
