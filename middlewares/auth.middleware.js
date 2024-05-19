0167f7487a2bdd3db60f271b4fdf0290bc56395b
AYUSH MISHRA <ayushm185@gmail.com>
2024-05-19 13:21:44 +0530

    finding error in auth middlewaare

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken'
 const isLoogedIn=async(req,res,next)=>{
      
      const {token}=req.cookies
+     console.log(token);
 
      if(!token){
         return next(new AppError("Not login in",401))
